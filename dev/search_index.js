69cc17a19c13a9393afa31259ff31c7c342175fc
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-30 23:54:39 +0000

    build based on ff7362d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Dilemma.jl","page":"Home","title":"Dilemma.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A Julia package to develop and evaluate context-free and contextual Multi-Armed Bandit policies","category":"page"}]
+}
